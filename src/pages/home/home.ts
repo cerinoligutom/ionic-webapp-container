83dd05fca5e047f08860d6efc45d248e256c75ac
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-05-04 15:46:52 +0800

    Used inappbrowser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 
 @Component({
   selector: 'page-home',
@@ -9,10 +11,19 @@ export class HomePage {
   url: string = 'http://192.168.';
 
   constructor(
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    private iab: InAppBrowser
   ) {}
 
   goToUrl() {
-    window.open(this.url, '_self', 'location=no,toolbar=no,hardwareback=no');
+    const browser = this.iab.create(
+      this.url,
+      '_self',
+      {
+        location: 'no',
+        toolbar: 'no',
+        hardwareback: 'no'
+      }
+    );
   }
 }
